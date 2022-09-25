6394d051ee091b052e009d01b9ed81d1e0fe9cf6
“sudo” <3048593912@qq.com>
2022-09-25 12:14:03 +0800

    Site updated: 2022-09-25 12:14:03

--- js/duration.js
+++ js/duration.js
@@ -22,8 +22,8 @@
     if(String(snum).length === 1 ){
       snum = "0" + snum;
     }
-    document.getElementById("timeDate").innerHTML = "已经运行&nbsp"+dnum+"&nbsp天呐";
-    document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
+    document.getElementById("timeDate").innerHTML = "已经运行&nbsp"+dnum+"&nbsp天";
+    document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒呐";
   }
 
   update();
